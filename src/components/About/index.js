40c928ff2750cc170c93374e072901b6344c4f68
Neo Yeh(葉秉豐) <Neo_Yeh@htc.com>
2021-03-09 17:48:43 +0800

    update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import moment from "moment";
 
 
 
@@ -50,37 +50,37 @@ const About = () => {
             years : '2020',
             protfolio_list : [
                 {
-                    name:'vive-homepage-revamp',
+                    name:'VIVE Homepage',
                     image:'./img/2020-home-page-revamp.jpg',
                     link_live:'https://www.vive.com/tw/',
                     link_github:'#',
                 },
                 {
-                    name:'aorus',
+                    name:'Aorus',
                     image:'./img/2020-pdp-aorus.jpg',
                     link_live:'#',
                     link_github:'#',
                 },
                 {
-                    name:'htc-desire20pro',
+                    name:'HTC Desire 20 Pro',
                     image:'./img/2020-pdp-desire20pro.jpg',
                     link_live:'https://www.htc.com/tw/smartphones/htc-desire-20-pro/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-sirocco',
+                    name:'HTC Desire 20 Plus',
                     image:'./img/2020-pdp-sirocco.jpg',
                     link_live:'https://www.htc.com/tw/smartphones/htc-desire-20-plus/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-u20-5g',
+                    name:'HTC U20 5G',
                     image:'./img/2020-pdp-u20-5g.jpg',
                     link_live:'https://www.htc.com/tw/smartphones/htc-u20-5g/',
                     link_github:'#',
                 },
                 {
-                    name:'vive-bf-20',
+                    name:'VIVE BlackFriday 2020',
                     image:'./img/2020-promotion-bf-20.jpg',
                     link_live:'#',
                     link_github:'#',
@@ -91,37 +91,37 @@ const About = () => {
             years : '2019',
             protfolio_list : [
                 {
-                    name:'vive-cosmos-2019',
+                    name:'VIVE COSMOS',
                     image:'./img/2019-pdp-cosmos-2019.jpg',
                     link_live:'https://www.vive.com/us/product/vive-cosmos/features/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-desire19s',
+                    name:'HTC Desire 19s',
                     image:'./img/2019-pdp-desire19s.jpg',
                     link_live:'https://www.htc.com/tw/smartphones/htc-desire-19s/',
                     link_github:'#',
                 },
                 {
-                    name:'vive-vrs',
+                    name:'VIVE Reality System 2019',
                     image:'./img/2019-pdp-vive-vrs.jpg',
                     link_live:'https://www.vive.com/us/vive-reality-system/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-wildfire-e',
+                    name:'HTC Wildfire e',
                     image:'./img/2019-pdp-wildfire-e.jpg',
                     link_live:'https://www.htc.com/af/smartphones/htc-wildfire-e/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-wildfire-e1',
+                    name:'HTC Wildfire e1',
                     image:'./img/2019-pdp-wildfire-e1.jpg',
                     link_live:'https://www.htc.com/af/smartphones/htc-wildfire-e1-wildfire-e1-plus/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-wildfire-op75',
+                    name:'HTC Wildfire OP75',
                     image:'./img/2019-pdp-wildfire-op75.jpg',
                     link_live:'https://www.htc.com/in/smartphones/htc-wildfire-r70/',
                     link_github:'#',
@@ -129,8 +129,10 @@ const About = () => {
             ]
         }
     ])
+    const years = moment().diff('1991-08-08', 'years');
     useEffect(()=> {
         console.log('render 後執行');
+        console.log(years);
         return () => {
             console.log(`render 移除後`);
         }
@@ -146,34 +148,7 @@ const About = () => {
     
     return (
         <div className="protfolio-content">
-            {list.map((list,index)=>{
-                return(
-                    <div className="protfolio-list" key={index}>
-                        <div className="protfolio-year">{list.years}</div>
-                        {list.protfolio_list.map((e,i)=>{
-                            return(
-                                <div className={"protfolio-card"} key={i}>
-                                    <div className="protfolio-card-padding">
-                                        <div className="protfolio-card-content">
-                                            <div className="image-block">
-                                                <img src={e.image} />
-                                            </div>
-                                            <div className="link-block">
-                                                <a href={e.link_live} target="_blank" rel="nofollow noopener noreferrer">
-                                                    live site
-                                                </a>
-                                                <a href={e.link_github} target="_blank" rel="nofollow noopener noreferrer">
-                                                    local site
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                )
-            })}
+            {years}
         </div>
     )
 };
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -45,7 +45,7 @@ const Portfolio = () => {
             years : '2021',
             protfolio_list : [
                 {
-                    name:'HTC desire 20plus',
+                    name:'HTC Desire 20 Plus',
                     image:'https://www.htc.com/media/filer_public/htc/shared/homepage/zh-tw/2020/sirocco/20201006_sirocco_homebanner_mobile.jpg',
                     link_live:'https://www.htc.com/tw/smartphones/htc-desire-20-plus/',
                     link_github:'#',
@@ -86,37 +86,37 @@ const Portfolio = () => {
             years : '2020',
             protfolio_list : [
                 {
-                    name:'vive-homepage-revamp',
+                    name:'VIVE Homepage',
                     image:'./img/2020-home-page-revamp.jpg',
                     link_live:'https://www.vive.com/tw/',
                     link_github:'#',
                 },
                 {
-                    name:'aorus',
+                    name:'Aorus',
                     image:'./img/2020-pdp-aorus.jpg',
                     link_live:'#',
                     link_github:'#',
                 },
                 {
-                    name:'htc-desire20pro',
+                    name:'HTC Desire 20 Pro',
                     image:'./img/2020-pdp-desire20pro.jpg',
                     link_live:'https://www.htc.com/tw/smartphones/htc-desire-20-pro/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-sirocco',
+                    name:'HTC Desire 20 Plus',
                     image:'./img/2020-pdp-sirocco.jpg',
                     link_live:'https://www.htc.com/tw/smartphones/htc-desire-20-plus/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-u20-5g',
+                    name:'HTC U20 5G',
                     image:'./img/2020-pdp-u20-5g.jpg',
                     link_live:'https://www.htc.com/tw/smartphones/htc-u20-5g/',
                     link_github:'#',
                 },
                 {
-                    name:'vive-bf-20',
+                    name:'VIVE BlackFriday 2020',
                     image:'./img/2020-promotion-bf-20.jpg',
                     link_live:'#',
                     link_github:'#',
@@ -127,51 +127,51 @@ const Portfolio = () => {
             years : '2019',
             protfolio_list : [
                 {
-                    name:'vive-cosmos-2019',
+                    name:'VIVE COSMOS',
                     image:'./img/2019-pdp-cosmos-2019.jpg',
                     link_live:'https://www.vive.com/us/product/vive-cosmos/features/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-desire19s',
+                    name:'HTC Desire 19s',
                     image:'./img/2019-pdp-desire19s.jpg',
                     link_live:'https://www.htc.com/tw/smartphones/htc-desire-19s/',
                     link_github:'#',
                 },
                 {
-                    name:'vive-vrs',
+                    name:'VIVE Reality System 2019',
                     image:'./img/2019-pdp-vive-vrs.jpg',
                     link_live:'https://www.vive.com/us/vive-reality-system/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-wildfire-e',
+                    name:'HTC Wildfire e',
                     image:'./img/2019-pdp-wildfire-e.jpg',
                     link_live:'https://www.htc.com/af/smartphones/htc-wildfire-e/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-wildfire-e1',
+                    name:'HTC Wildfire e1',
                     image:'./img/2019-pdp-wildfire-e1.jpg',
                     link_live:'https://www.htc.com/af/smartphones/htc-wildfire-e1-wildfire-e1-plus/',
                     link_github:'#',
                 },
                 {
-                    name:'htc-wildfire-op75',
+                    name:'HTC Wildfire OP75',
                     image:'./img/2019-pdp-wildfire-op75.jpg',
                     link_live:'https://www.htc.com/in/smartphones/htc-wildfire-r70/',
                     link_github:'#',
                 }
             ]
         }
-    ])
+    ]);
     useEffect(()=> {
         console.log('render 後執行');
         return () => {
             console.log(`render 移除後`);
         }
         //放空陣列只會在render後跑一次
-    },[])
+    },[]);
     useEffect(()=> {
         console.log(`state改變成 ${list}`);
         console.log(`========`);
@@ -179,7 +179,7 @@ const Portfolio = () => {
         return () => {
             console.log(`state改變前 ${list}`);
         }
-    },[list])
+    },[list]);
     
     return (
         <div className="protfolio-content">
@@ -187,27 +187,30 @@ const Portfolio = () => {
                 return(
                     <div className="protfolio-list" key={index}>
                         <div className="protfolio-year">{list.years}</div>
-                        {list.protfolio_list.map((e,i)=>{
-                            return(
-                                <div className={"protfolio-card"} key={i}>
-                                    <div className="protfolio-card-padding">
-                                        <div className="protfolio-card-content">
-                                            <div className="image-block">
-                                                <img src={e.image} />
-                                            </div>
-                                            <div className="link-block">
-                                                <a href={e.link_live} target="_blank" rel="nofollow noopener noreferrer">
-                                                    live site
-                                                </a>
-                                                <a href={e.link_github} target="_blank" rel="nofollow noopener noreferrer">
-                                                    local site
-                                                </a>
+                        <div className="protfolio-list-content">
+                            {list.protfolio_list.map((e,i)=>{
+                                return(
+                                    <div className={"protfolio-card"} key={i}>
+                                        <div className="protfolio-card-padding">
+                                            <div className="protfolio-card-content">
+                                                <div className="image-block">
+                                                    <img src={e.image} />
+                                                </div>
+                                                <div className="link-block">
+                                                    <div className="name font-card-title">{e.name}</div>
+                                                    <a href={e.link_live} className="font-card-text" target="_blank" rel="nofollow noopener noreferrer">
+                                                        Learn More On Live Site
+                                                    </a>
+                                                    <a href={e.link_github} className="font-card-text" target="_blank" rel="nofollow noopener noreferrer">
+                                                    Learn More On GitHub
+                                                    </a>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })}
+                                )
+                            })}
+                        </div>
                     </div>
                 )
             })}
